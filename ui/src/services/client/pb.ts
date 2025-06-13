3bbaeba7dd4b4b3b3d451639bb0ed87d5e1478e1
Kevin Saucedo <user002_@outlook.com>
2025-06-12 23:35:44 -0500

    fix embed api and base url

--- ui/src/services/client/pb.ts
+++ ui/src/services/client/pb.ts
@@ -1,3 +1,3 @@
 import PocketBase from "pocketbase";
-const api_url = import.meta.env.VITE_API_URL ?? "/";
+const api_url = import.meta.env.VITE_API_URL?.trim() || "/";
 export const pb = new PocketBase(api_url);
